ee1f9c151d550b0e345521008a37249680c4e604
Chad.Snyder <Mapletonkid@gmail.com>
2020-01-27 15:38:20 -0700

    mvp

---
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {render, fireEvent, wait, findAllByTestId} from '@testing-library/react';
+import StarWarsCharacters from "./StarWarsCharacters";
+import {getData as mockGetData} from '../api'
+
+jest.mock("../api")
+
+// test("app renders without crashing", () =>{
+//   render(<StarWarsCharacters/>);
+// })
+
+// test ('buttoms render to dom', () =>{
+//   const {getByText} = render(<StarWarsCharacters/>);
+
+//   getByText(/next/i);
+//   getByText(/previous/i);
+// })
+
+test('buttons fire and and call correct functions', async () =>{
+  mockGetData.mockResolvedValueOnce(
+    {results: [{
+      name: ""
+    }],
+    next:"abcd",
+    previous:"now"
+
+    }
+  );
+  const {getByText} = render(<StarWarsCharacters/>);
+  const nextButton = getByText(/next/i);
+  const prevButton = getByText(/previous/i);
+
+  fireEvent.click(nextButton);
+  fireEvent.click(prevButton);
+  expect(mockGetData).toHaveBeenCalledTimes(1);
+
+  wait(() => expect(getByText(/'Darth'/i)));
+})
